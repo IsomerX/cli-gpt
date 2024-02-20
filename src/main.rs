1e01b91f13cc2a2b9d0b64badfebde73cbf6df16
IsomerX <49413460+IsomerX@users.noreply.github.com>
2024-02-20 11:46:41 +0530

    colorfulll

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,86 @@
 use clap::Parser;
+use colored::Colorize;
 use openai_api_rs::v1::api::Client;
 use openai_api_rs::v1::chat_completion::{self, ChatCompletionRequest};
-use openai_api_rs::v1::common::GPT3_5_TURBO;
-use std::env;
+use openai_api_rs::v1::common::{GPT3_5_TURBO, GPT4};
+use termimad::crossterm::style::Color::*;
+use termimad::*;
 
 #[derive(Parser)]
 struct Cli {
-    questions: String,
+    gpt_version: String,
+    open_ai_key: String,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client = Client::new(env::var("OPENAI_API_KEY").unwrap().to_string());
     let args = Cli::parse();
-    let req = ChatCompletionRequest::new(
-        GPT3_5_TURBO.to_string(),
+    let client = Client::new(args.open_ai_key);
+    let mut gpt_version: String = GPT3_5_TURBO.to_string();
+    let mut skin = MadSkin::default();
+    match args.gpt_version.as_str() {
+        "3.5" => {
+            gpt_version = GPT3_5_TURBO.to_string();
+        }
+        "4" => {
+            gpt_version = GPT4.to_string();
+        }
+        _ => {
+            println!("Invalid GPT version. Using GPT-3.5 turbo by default.");
+        }
+    }
+
+    let mut messages: Vec<chat_completion::ChatCompletionMessage> =
         vec![chat_completion::ChatCompletionMessage {
             role: chat_completion::MessageRole::user,
-            content: chat_completion::Content::Text(String::from(args.questions)),
+            content: chat_completion::Content::Text("Hello, how are you?".to_string()),
             name: None,
-        }],
-    );
-    let result = client.chat_completion(req)?;
-    println!("{:?}", result.choices[0].message.content);
+        }];
+
+    loop {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input)?;
+        match input.trim() {
+            "exit" => break,
+            "cls" => {
+                messages.clear();
+                print!("{esc}c", esc = 27 as char);
+                println!("{}", "Chat history cleared.".green());
+            }
+            "clh" => {
+                messages.clear();
+                println!("{}", "Chat history cleared.".green());
+            }
+            _ => {
+                messages.push(chat_completion::ChatCompletionMessage {
+                    role: chat_completion::MessageRole::user,
+                    content: chat_completion::Content::Text(input.trim().to_string()),
+                    name: None,
+                });
+                let req = ChatCompletionRequest::new(gpt_version.clone(), messages.clone());
+                let result = client.chat_completion(req)?;
+                match &result.choices[0].message.content {
+                    Some(content) => {
+                        messages.push(chat_completion::ChatCompletionMessage {
+                            role: chat_completion::MessageRole::assistant,
+                            content: chat_completion::Content::Text(content.to_string()),
+                            name: None,
+                        });
+                        let mut markdown = String::new();
+                        skin.set_headers_fg(Yellow);
+                        skin.bold.set_fg(Yellow);
+                        skin.italic.set_fgbg(Magenta, rgb(30, 30, 40));
+                        skin.paragraph.set_fg(Yellow);
+
+                        markdown.push_str(&content.yellow());
+
+                        println!("{}\n", skin.term_text(&markdown.yellow()));
+                    }
+                    None => {
+                        println!("{}", "No response from GPT".red());
+                    }
+                }
+            }
+        }
+    }
     Ok(())
 }
